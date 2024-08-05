c89f6bb3b61908d364b40488445462f12db1e713
bimponda blaise <bimpondab@gmail.com>
2024-08-05 16:41:26 +0200

    first project

---
+++ page.js
@@ -0,0 +1,27 @@
+let table = 100;
+console.log(table);
+
+let tabl = 20;
+console.log(tabl);
+
+const sac = "red";
+console.log(sac);
+
+const blaise = "blaise à un sac";
+console.log(blaise + " " + sac);
+
+let tt = false
+
+let tableau = [2, 30, 23, 1, "blaise"];
+console.log(tableau);
+console.log(tableau[3] + tableau[2]);
+
+
+
+
+
+// classe exemple la description d'un personne
+// structure conditionnel
+// boucles
+// methode map, filter
+
